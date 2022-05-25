d8be6e012d513fb21d5304d40171d21816d7a115
Jiwoo Yang <65417437+didwldn3032@users.noreply.github.com>
2022-05-25 13:41:19 +0900

    [del] 잘못된 class 삭제

--- src/item.py
+++ src/item.py
@@ -150,25 +150,4 @@ class obj(pygame.sprite.Sprite):
 
 
 
-class Umbrella(pygame.sprite.Sprite):
 
-    def __init__(self, speed=5, sizex=-1, sizey=-1):
-        pygame.sprite.Sprite.__init__(self, self.containers)
-        self.images, self.rect = load_sprite_sheet('umbrella_item.png', 1, 1, sizex, sizey, -1)
-        self.rect.centery = 0
-        self.image = self.images[0]
-        self.movement = [speed, 0]
-        self.index = 0
-        self.counter = 0
-
-    def draw(self):
-        screen.blit(self.image, self.rect)
-
-    def update(self):
-        if self.counter % 10 == 0:
-            self.index = (self.index + 1) % 2
-        self.image = self.images[self.index]
-        self.rect = self.rect.move(self.movement)
-        self.counter = (self.counter + 1)
-        if self.rect.right < 0:
-            self.kill()
